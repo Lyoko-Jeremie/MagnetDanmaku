bc50111ef62faab92d8e22d2fee2f324e7aab789
Jeremie <lucheng989898@gmail.com>
2016-02-15 14:29:10 +0800

    videohash

--- test/danma_factory_test.js
+++ test/danma_factory_test.js
@@ -7,8 +7,12 @@ const seed = sodium.randombytes_buf(sodium.crypto_sign_SEEDBYTES);
 
 //console.log(seed);
 
+const videohash = sodium.to_base64(
+    sodium.crypto_generichash(64, "videohashvideohashvideohash")
+);
+
 const dama = fac.createDanmaFromStrngTypeDanmaContent(
-    "content", 123451346, 0xFFFFFFFF, "videohashvideohashvideohash", seed
+    "content", 123451346, 0xFFFFFFFF, videohash, seed
 );
 console.log(dama);
 
