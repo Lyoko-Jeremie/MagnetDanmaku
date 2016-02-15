3d1bb2ee046af148fc7eb91c54d6e5587a1b68e1
Jeremie <lucheng989898@gmail.com>
2016-02-15 19:26:35 +0800

    damapoolmanager已写好 TODO damapool

---
+++ test/poolmanager_test.js
@@ -0,0 +1,31 @@
+import poolmanager from "../lib/poolmanager-compiled";
+
+
+var pm = new poolmanager.damapoolmanager();
+
+
+//var PoolList = {
+//    "b": 0,
+//    "a": 0,
+//    "z": 0,
+//    "c": 0
+//};
+//PoolList["x"] = 1;
+//for (var i in PoolList) {
+//    console.log(i + ":" + PoolList[i]);
+//}
+//
+//console.log(5 + ":" + PoolList[5]);
+//console.log(5 in PoolList);
+//console.log("a" in PoolList);
+//
+//delete PoolList["a"];
+//
+//for (var i in PoolList) {
+//    console.log(i + ":" + PoolList[i]);
+//}
+
+
+
+
+
